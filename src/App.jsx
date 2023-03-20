357e2c08f41a20adb54d4dc92619ff30dd5ea5b8
Leonardo <leo11madara@gmail.com>
2023-03-20 18:13:14 -0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
       <Router>
 
         <Routes>
+          <Route path='/' element={<ListaProdutos />} />
           <Route path='/produtos' element={<ListaProdutos />} />
           <Route path='/carrinho' element={<Carrinho />} />
           <Route path='/carrinho/confirmacao' element={<TelaConfirmacao />} />
